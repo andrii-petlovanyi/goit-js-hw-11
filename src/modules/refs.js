805a9b0b10b654464045b6d86815f40c12a0e05f
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-10-25 13:29:05 +0300

    added loader for infinity scroll

--- src/modules/refs.js
+++ src/modules/refs.js
@@ -1,12 +1,12 @@
 const refs = {
   formSubmit: document.querySelector('#search-form'),
   input: document.querySelector('[name="searchQuery"]'),
-  gallery: document.querySelector('.gallery'),
-  toTopBtn: document.querySelector('.top-btn'),
-  header: document.querySelector('.header'),
-  submitBtn: document.querySelector('.submit'),
-  modalImg: document.querySelector('.mod'),
-  loader: document.querySelector('.loader'),
+  gallery: document.querySelector('.gallery-js'),
+  toTopBtn: document.querySelector('.top-btn-js'),
+  header: document.querySelector('.header-js'),
+  submitBtn: document.querySelector('.submit-js'),
+  modalImg: document.querySelector('.mod-js'),
+  loader: document.querySelector('.loader-js'),
 };
 
 export default refs;
